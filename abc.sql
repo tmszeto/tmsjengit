6bd0b5a470edc1200dccd6752ad7a2f9031bf2f7
T.M. Szeto <tm.szeto@fuhu.com>
2014-02-04 15:57:38 -0800

    Committing these four new files

---
+++ abc.sql
@@ -0,0 +1,3 @@
+00 Original contents of this file are preceded by 00
+00 INSERT INTO table_children(FirstName, LastName) VALUES ("Mike", "Leung");
+00 INSERT INTO table_children(FirstName, LastName) VALUES ("Marry", "Leung");
