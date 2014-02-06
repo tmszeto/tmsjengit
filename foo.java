042261714d7f455de4970f9b32de04fab0f48c62
T.M. Szeto <tm.szeto@fuhu.com>
2014-02-06 14:21:43 -0800

    Removing foo.java and foo.can permanently

--- foo.java
+++
@@ -1,8 +0,0 @@
-//00 Original contents of this file are preceded by 00
-/** 00  */ public class foo {
-/** 00  */ 
-/** 00  */     // method main(): ALWAYS the APPLICATION entry point
-/** 00  */     public static void main (String[] args) {
-/** 00  */ 	System.out.println ("Hello World!");
-/** 00  */     }
-/** 00  */ }
