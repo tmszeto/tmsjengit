eee9679c7f15c7a347b98928a85e666007d5f0e0
T.M. Szeto <tm.szeto@fuhu.com>
2014-02-06 13:57:27 -0800

    Adding two new files and one modified file

---
+++ hello.java
@@ -0,0 +1,9 @@
+//00 Original contents of this file are preceded by 00
+//01 The contents of Version 01 of this file are preceded by 01
+/** 01  */ public class hello {
+/** 01  */ 
+/** 01  */     // method main(): ALWAYS the APPLICATION entry point
+/** 01  */     public static void main (String[] args) {
+/** 01  */ 	System.out.println ("Hello World!");
+/** 01  */     }
+/** 01  */ }
